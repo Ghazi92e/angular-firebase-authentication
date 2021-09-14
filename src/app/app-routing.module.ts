26f8c9041900e78a2b1ccc6a32f1430bfe7efaf9
Ghazi <ghaziatie@gmail.com>
2021-09-14 11:45:23 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { BookFormComponent } from './components/book-list/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SingleBookComponent } from './components/book-list/single-book/single-book.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'email-verification', component: VerifyEmailComponent },
+  { path: 'books', component: BookListComponent, canActivate: [AuthGuard] },
+  { path: 'books/new', component: BookFormComponent, canActivate: [AuthGuard] },
+  { path: 'books/view/:id', component: SingleBookComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
