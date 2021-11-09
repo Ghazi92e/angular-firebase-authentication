609fec42e4ce411b2c95954fec5558646b43448c
Ghazi <ghaziatie@gmail.com>
2021-11-09 10:21:28 +0100

    remove function delete book

--- src/app/layout/book-item/book-item.component.ts
+++ src/app/layout/book-item/book-item.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Book } from 'src/app/_models/Book.model';
-import { BooksService } from 'src/app/_services/books.service';
-import { UploadFileService } from 'src/app/_services/upload-file.service';
-
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
@@ -12,14 +9,8 @@ export class BookItemsComponent implements OnInit {
 
   @Input() book: Book | any;
 
-  constructor(private booksService: BooksService, private uploadService: UploadFileService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-  onDeleteBook(book: Book) {
-    this.booksService.removeBook(book);
-    this.uploadService.deleteFileUpload(book)
-  }
-
 }
