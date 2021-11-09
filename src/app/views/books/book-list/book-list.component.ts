272145d2fb97e0a460ee8a0c9c3b7064a5ac4fd7
Ghazi <ghaziatie@gmail.com>
2021-11-09 10:25:33 +0100

    add feature addidbooktoUser

--- src/app/views/books/book-list/book-list.component.ts
+++ src/app/views/books/book-list/book-list.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
 import { Book } from 'src/app/_models/Book.model';
+import { User } from 'src/app/_models/User.model';
 import { BooksService } from 'src/app/_services/books.service';
+import { NgAuthService } from 'src/app/_services/ng-auth.service';
 import { UploadFileService } from 'src/app/_services/upload-file.service';
+import { UsersService } from 'src/app/_services/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-book-list',
@@ -14,12 +18,28 @@ import { UploadFileService } from 'src/app/_services/upload-file.service';
 export class BookListComponent implements OnInit, OnDestroy {
 
   books: Book[] = []; //arret local
+  user: User | any;
+  addidbook: string[] = [];
   booksSubscription: Subscription = new Subscription;
-
+  userSubscription: Subscription = new Subscription;
   
-  constructor(private booksService: BooksService, private uploadService: UploadFileService, private router: Router, private SpinnerService: NgxSpinnerService) { }
+  constructor(private booksService: BooksService,
+              private uploadService: UploadFileService,
+              private router: Router, 
+              private SpinnerService: NgxSpinnerService, 
+              private userService: UsersService ) { }
 
   ngOnInit(): void {
+    this.SpinnerService.show();
+    this.user = new User();
+    this.userSubscription = this.userService.userSubject.subscribe(
+      (user: User) => {
+        this.user = user;
+        this.SpinnerService.hide();
+      }
+    );
+    this.userService.getUser();
+
     this.SpinnerService.show();
     this.booksSubscription = this.booksService.booksSubject.subscribe(
       (books: Book[]) => {
@@ -28,7 +48,32 @@ export class BookListComponent implements OnInit, OnDestroy {
       }
     );
     this.booksService.getBooks();
-    /*this.booksService.emitBooks();*/
+  }
+
+  delbook(indexbook: number) {
+    const index = this.user.bookids.indexOf(indexbook);
+    if (index > -1) {
+      this.user.bookids.splice(index, 1);
+      this.userService.removeBookUser(index);
+    }
+    this.router.navigate(['/books']);
+    Swal.fire('Bravo !', "Votre livre a bien été supprimé", 'success');
+  }
+
+  addidbooktoUser(id: string) {
+    if (this.user.bookids) {
+      for (var value of this.user.bookids) {
+        if (value) {
+          this.addidbook.push(value);
+          console.log(value);
+        }
+      }
+    }
+    this.addidbook.push(id);
+    console.log(this.addidbook);
+    this.userService.addbookidsUser(this.addidbook);
+    this.addidbook = [];
+    Swal.fire('Bravo !', "Votre livre a bien été ajouté", 'success');
   }
 
   onNewBook() {
@@ -39,8 +84,14 @@ export class BookListComponent implements OnInit, OnDestroy {
     this.router.navigate(['/books', 'view', id]);
   }
 
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+    this.uploadService.deleteFileUpload(book)
+  }
+
   ngOnDestroy() {
     this.booksSubscription.unsubscribe();
   }
 
 }
+
