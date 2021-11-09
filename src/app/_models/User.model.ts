81b711fbffa7756a147036279cf6c835d8da53ee
Ghazi <ghaziatie@gmail.com>
2021-11-09 10:19:24 +0100

    remove id

--- src/app/_models/User.model.ts
+++ src/app/_models/User.model.ts
@@ -1,8 +1,6 @@
 export class User {
-    id: string | any;
-    name: string | any;
-    email: string | any;
-    
+    name: string | undefined;
+    email: string | undefined;
     constructor() {
     }
 }
