0431258f5652ddff11c1ede3ded6ffd536923f54
Ghazi <ghaziatie@gmail.com>
2021-11-16 10:38:57 +0100

    refacto function addidbooktoUser

--- src/app/views/books/book-list/book-list.component.ts
+++ src/app/views/books/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
 import { Book } from 'src/app/_models/Book.model';
@@ -9,6 +9,7 @@ import { NgAuthService } from 'src/app/_services/ng-auth.service';
 import { UploadFileService } from 'src/app/_services/upload-file.service';
 import { UsersService } from 'src/app/_services/users.service';
 import Swal from 'sweetalert2';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-book-list',
@@ -17,28 +18,38 @@ import Swal from 'sweetalert2';
 })
 export class BookListComponent implements OnInit, OnDestroy {
 
-  books: Book[] = []; //arret local
-  user: User | any;
-  addidbook: string[] = [];
+  books: Book[] = [];
+  user: User = new User();
   booksSubscription: Subscription = new Subscription;
   userSubscription: Subscription = new Subscription;
-  
+  useremail: string | any;
+  displayName: string | any;
+
   constructor(private booksService: BooksService,
               private uploadService: UploadFileService,
               private router: Router, 
-              private SpinnerService: NgxSpinnerService, 
-              private userService: UsersService ) { }
+              private SpinnerService: NgxSpinnerService,
+              public ngAuthService: NgAuthService, 
+              private userService: UsersService,
+              public afAuth: AngularFireAuth ) {
+              }
 
   ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => {
+      if (user && user.uid) {
+        this.displayName = user.displayName;
+        this.useremail = user.email;
+        this.userService.getUser(user.uid);
+      }
+    });
+
     this.SpinnerService.show();
-    this.user = new User();
     this.userSubscription = this.userService.userSubject.subscribe(
       (user: User) => {
         this.user = user;
         this.SpinnerService.hide();
       }
     );
-    this.userService.getUser();
 
     this.SpinnerService.show();
     this.booksSubscription = this.booksService.booksSubject.subscribe(
@@ -51,18 +62,15 @@ export class BookListComponent implements OnInit, OnDestroy {
   }
 
   addidbooktoUser(id: string) {
-    if (this.user.bookids) {
-      for (var value of this.user.bookids) {
-        if (value) {
-          this.addidbook.push(value);
-          console.log(value);
-        }
-      }
+    if (this.user.bookids == null) {
+      this.user.bookids = [];
+      this.user.bookids.push(id);
+    } else {
+      this.user.bookids.push(id);
     }
-    this.addidbook.push(id);
-    console.log(this.addidbook);
-    this.userService.addbookidsUser(this.addidbook);
-    this.addidbook = [];
+    this.user.name = this.displayName;
+    this.user.email = this.useremail;
+    this.userService.updateUser(this.user);
     Swal.fire('Bravo !', "Votre livre a bien été ajouté", 'success');
   }
 
