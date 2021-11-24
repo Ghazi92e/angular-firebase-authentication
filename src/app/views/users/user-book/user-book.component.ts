b8ba91201c555d61aa02289c89d47035cf99c5c8
Ghazi <ghaziatie@gmail.com>
2021-11-24 11:10:51 +0100

    add firestore

--- src/app/views/users/user-book/user-book.component.ts
+++ src/app/views/users/user-book/user-book.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { Subscription } from 'rxjs';
-import { User } from 'src/app/_models/User.model';
+import { Book } from 'src/app/_models/Book.model';
+import { BooksService } from 'src/app/_services/books.service';
+import { User } from 'src/app/_services/ng-auth.service';
 import { UsersService } from 'src/app/_services/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-book',
@@ -12,39 +15,64 @@ import { UsersService } from 'src/app/_services/users.service';
 })
 export class UserBookComponent implements OnInit {
   
-  user: User = new User();
-  userSubscription: Subscription = new Subscription;
+  user: User
+  books: Book[] = []
+  useruid: string
+  idbook: string[] = []
 
   constructor(private userService: UsersService, 
               private SpinnerService: NgxSpinnerService,
-              public afAuth: AngularFireAuth) { }
+              private booksService: BooksService,
+              private router: Router,
+              public afAuth: AngularFireAuth) 
+              {
+                this.user = {
+                  uid: '',
+                  email: '',
+                  displayName: '',
+                  photoURL: '',
+                  emailVerified: false,
+                  bookids: []
+                };
+                this.useruid = ''
+              }
 
   ngOnInit(): void {
     this.afAuth.authState.subscribe(user => {
       if (user && user.uid) {
-        this.userService.getUser(user.uid);
+        this.useruid = user.uid
+        this.getDataUser(this.useruid)
       }
     });
 
+    // this.booksService.getBooksFirestore().then((data) => {
+    //   data.forEach((docbookid) => {
+    //     this.idbook.push(docbookid.id)
+    //   })
+    // })
+  }
+
+  getDataUser(userid: string) {
     this.SpinnerService.show()
-    this.userSubscription = this.userService.userSubject.subscribe(
-      (user: User) => {
-        this.user = user;
-        this.SpinnerService.hide();
+    this.userService.getUserFirestore(userid).subscribe(
+      (user) => {
+        this.user = user.data()!;
+        for (const value of this.user.bookids){
+          this.booksService.getSingleBookFirestore(value).then((doc) => {
+            this.books.push(doc.data()!)
+          })
+        }
+        this.SpinnerService.hide()
       }
     );
   }
 
-  removeBook(idbookindex: number) {
-    this.userService.removeBookUser(idbookindex);
-    if (this.user.bookids) {
-      this.user.bookids = this.user.bookids.join('').split('');
-      this.userService.updateUser(this.user);
-    };
+  removeUserBook(idbookindex: number) {
+    if (idbookindex > -1) {
+      this.books.splice(idbookindex, 1);
+      this.user.bookids.splice(idbookindex, 1);
+    }
+    this.userService.removebookUserfirestore(this.user.bookids, this.useruid);
+    Swal.fire('Bravo !', "Votre livre a bien été supprimé", 'success');
   }
-
-  ngOnDestroy() {
-    this.userSubscription.unsubscribe();
-  }
-
 }
