ed1b91621ab6df0b1aa885e50393ebe05c3df633
Ghazi <ghaziatie@gmail.com>
2021-11-24 11:05:15 +0100

    add firestore

--- src/app/_services/users.service.ts
+++ src/app/_services/users.service.ts
@@ -1,47 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { AngularFireDatabase } from "@angular/fire/compat/database";
-import { User } from '../_models/User.model';
-import { NgAuthService } from './ng-auth.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { User } from './ng-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  userFire: AngularFirestoreCollection<User>;
+  dbPath = 'users';
 
-  user: User = new User();
-  userSubject = new Subject<User>();
+  constructor( private afStore: AngularFirestore ) { this.userFire = this.afStore.collection(this.dbPath) }
 
-  constructor(
-    private afDatabase: AngularFireDatabase,
-    private ngAuthService: NgAuthService
-
-  ) { }
-
-  emitUser() {
-    this.userSubject.next(this.user);
+  getUserFirestore(userid: string) {
+    return this.userFire.doc(userid).get();
   }
 
-  updateUser(user: User) {
-    this.afDatabase.database.ref('/users/' + this.ngAuthService.userAuth.uid).set(user);
+  updateBookUser(bookids: string[], userid: string) {
+    this.userFire.doc(userid).update({bookids});
   }
 
-  getUser(useruid: string) {
-    this.afDatabase.database.ref('/users/' + useruid)
-    .on('value', (data) => {
-      this.user = data.val() ? data.val() : [];
-      this.emitUser();
-    })
-  }
-
-  removeBookUser(id: number) {
-    var adaRef = this.afDatabase.database.ref('/users/' + this.ngAuthService.userAuth.uid + '/bookids/' + id);
-    adaRef.remove()
-      .then(function() {
-        console.log("Remove succeeded.")
-      })
-      .catch(function(error) {
-        console.log("Remove failed: " + error.message)
-      });
+  removebookUserfirestore(bookids: string[], userid: string) {
+    this.userFire.doc(userid).update({bookids});
   }
 }
