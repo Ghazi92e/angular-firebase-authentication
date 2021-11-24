1778881819fef57d232dc8c00baa20011c75f4dd
Ghazi <ghaziatie@gmail.com>
2021-11-24 11:10:05 +0100

    refacto with firestore

--- src/app/views/books/single-book/single-book.component.ts
+++ src/app/views/books/single-book/single-book.component.ts
@@ -1,13 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { Subscription } from 'rxjs';
 import { Book } from 'src/app/_models/Book.model';
-import { User } from 'src/app/_models/User.model';
 import { BooksService } from 'src/app/_services/books.service';
-import { NgAuthService } from 'src/app/_services/ng-auth.service';
-import { UploadFileService } from 'src/app/_services/upload-file.service';
+import { NgAuthService, User} from 'src/app/_services/ng-auth.service';
 import { UsersService } from 'src/app/_services/users.service';
 import Swal from 'sweetalert2';
 
@@ -19,14 +16,12 @@ import Swal from 'sweetalert2';
 
 export class SingleBookComponent implements OnInit {
 
-  book: Book | any;
-  books: Book[] = [];
-  user: User = new User();
-  userSubscription: Subscription = new Subscription;
-  booksSubscription: Subscription = new Subscription;
-  useremail: string | any;
-  displayName: string | any;
-
+  book: Book
+  user: User
+  useremail: string
+  displayName: string
+  useruid: string
+  idbook: string[] = []
   id = this.route.snapshot.params['id'];
 
   constructor(private route: ActivatedRoute,
@@ -34,71 +29,72 @@ export class SingleBookComponent implements OnInit {
               private userService: UsersService,
               public ngAuthService: NgAuthService,
               private SpinnerService: NgxSpinnerService,
-              private uploadService: UploadFileService,
               public afAuth: AngularFireAuth,
-              private router: Router) {
+              private router: Router) 
+              {
+                this.user = {
+                  uid: '',
+                  email: '',
+                  displayName: '',
+                  photoURL: '',
+                  emailVerified: false,
+                  bookids: []
+                };
+                this.book = {
+                  title: '',
+                  author: '',
+                  url: ''
+                }
+                this.useruid = ''
+                this.displayName = ''
+                this.useremail = ''
               }
 
   ngOnInit() {
     this.afAuth.authState.subscribe(user => {
       if (user && user.uid) {
-        this.displayName = user.displayName;
-        this.useremail = user.email;
-        this.userService.getUser(user.uid);
+        this.useruid = user.uid;
+        this.displayName = user.displayName!;
+        this.useremail = user.email!;
+        this.getDataUser(this.useruid);
       }
     });
 
-    this.SpinnerService.show()
-    this.userSubscription = this.userService.userSubject.subscribe(
-      (user: User) => {
-        this.user = user;
+    this.booksService.getBooksFirestore().then((data) => {
+      data.forEach((docbookid) => {
+        this.idbook.push(docbookid.id)
+        this.SpinnerService.show()
+        this.booksService.getSingleBookFirestore(this.idbook[this.id]).then((doc) => {
+          this.book = doc.data()!
+          this.SpinnerService.hide()
+        })
+      })
+    })
+  }
+
+  getDataUser(userid: string) {
+    this.userService.getUserFirestore(userid).subscribe(
+      (user) => {
+        this.user = user.data()!;
         console.log(this.user);
-        this.SpinnerService.hide();
-      }
-    );
-
-    this.SpinnerService.show();
-    this.booksSubscription = this.booksService.booksSubject.subscribe(
-      (books: Book[]) => {
-        this.books = books;
-        this.SpinnerService.hide();
-      }
-    );
-    this.booksService.getBooks();
-
-    this.book = new Book('', ''); // crée un Book vide pour eviter les erreurs
-    const id = this.route.snapshot.params['id'];
-    this.booksService.getSingleBook(+id).then( // '+id' pour le cast en tant que number
-      (book) => {
-        this.book = book;
-        console.log(this.book);
       }
     );
   }
 
   delbook(indexbook: string) {
-    const index = this.user.bookids.indexOf(indexbook);
-    if (index > -1) {
-      this.userService.removeBookUser(index);
-    }
-    if (this.user.bookids) {
-      this.user.bookids = this.user.bookids.join('').split('');
-      this.userService.updateUser(this.user);
-    }
+    this.user.bookids.forEach((element, index)=>{
+      if(element == indexbook) {
+        this.user.bookids.splice(index, 1);
+      }
+    });
+    this.userService.removebookUserfirestore(this.user.bookids, this.useruid);
     this.router.navigate(['/books']);
     Swal.fire('Bravo !', "Votre livre a bien été supprimé", 'success');
   }
 
   addidbooktoUser(id: string) {
-    if (this.user.bookids == null) {
-      this.user.bookids = [];
-      this.user.bookids.push(id);
-    } else {
-      this.user.bookids.push(id);
-    }
-    this.user.name = this.displayName;
-    this.user.email = this.useremail;
-    this.userService.updateUser(this.user);
+    this.user.bookids.push(this.idbook[+id]);
+    this.userService.updateBookUser(this.user.bookids, this.useruid);
     this.router.navigate(['/books']);
     Swal.fire('Bravo !', "Votre livre a bien été ajouté", 'success');
   }
@@ -111,10 +107,4 @@ export class SingleBookComponent implements OnInit {
   onBack() {
     this.router.navigate(['/books']);
   }
-
-  ngOnDestroy() {
-    this.userSubscription.unsubscribe();
-    this.booksSubscription.unsubscribe();
-  }
-
 }
