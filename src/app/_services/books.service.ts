2d1770a38193acad7dc5b40be8ed96882654517a
Ghazi <ghaziatie@gmail.com>
2021-11-24 11:01:32 +0100

    add firestore

--- src/app/_services/books.service.ts
+++ src/app/_services/books.service.ts
@@ -1,78 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Book } from '../_models/Book.model';
-import { AngularFireDatabase } from "@angular/fire/compat/database";
-import { Subject } from 'rxjs';
-
-
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+  bookFire: AngularFirestoreCollection<Book>;
+  dbPath = 'books'
+  
+  constructor( private afStore: AngularFirestore ) { this.bookFire = this.afStore.collection(this.dbPath) }
 
-  books: Book[] = []; //arret local
-  booksSubject = new Subject<Book[]>();
-
-  constructor(
-    private afDatabase: AngularFireDatabase,
-
-  ) { }
-
-  emitBooks() {
-    this.booksSubject.next(this.books); // prend le contenu de l'arret books et l'emettra a travers le subject
+  getSingleBookFirestore(bookid: string) {
+    return this.bookFire.doc(bookid).ref.get();
   }
 
-
-  saveBooks() {
-    this.afDatabase.database.ref('/books').set(this.books); // this.books enregistré dans '/books'
+  getBooksFirestore() {
+    return this.bookFire.ref.get()
   }
 
-  getBooks() {
-    this.afDatabase.database.ref('/books')
-      .on('value', (data) => { // '.on' permet de réagir à des modification de la BDD. '(data) contient la propriété 'val' qui retourne la valeur des données du serveur 
-        this.books = data.val() ? data.val() : [];
-        this.emitBooks();
-      })
+  updateBook(id: string, book: Book) {
+    this.bookFire.doc(id).set(book);
   }
-
-  getSingleBook(id: number) {
-    return new Promise((resolve, reject) => {
-      this.afDatabase.database.ref('/books/' + id).once('value').then(
-        (data) => {
-          resolve(data.val());
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    });
-  }
-
-  updateBook(id: number, book: Book) {
-    this.afDatabase.database.ref('/books/' + id).update(book);
-    //this.saveBooks();
-    //this.emitBooks();
-  }
-
 
   createNewBook(newBook: Book) {
-    this.books.push(newBook);
-    this.saveBooks();
-    //this.emitBooks();
+    this.bookFire.add(newBook);
   }
 
-  removeBook(book: Book) {
-    const bookIndexToRemove = this.books.findIndex(
-      (bookEl) => {
-        if(bookEl === book) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    );
-    this.books.splice(bookIndexToRemove, 1);
-    this.saveBooks();
-    this.emitBooks();
+  deleteBookFirestore(id: string) {
+    this.bookFire.doc(id).delete()
   }
 }
