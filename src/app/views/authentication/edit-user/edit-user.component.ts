feee94592d2cbc3e215bada9e7dae83366e04e2d
Ghazi <ghaziatie@gmail.com>
2021-11-24 15:55:55 +0100

    add function to update username

--- src/app/views/authentication/edit-user/edit-user.component.ts
+++ src/app/views/authentication/edit-user/edit-user.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { NgAuthService } from 'src/app/_services/ng-auth.service';
+import { updateProfile } from "firebase/auth";
+import Swal from 'sweetalert2';
+import { UsersService } from 'src/app/_services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-user',
@@ -7,10 +12,26 @@ import { NgAuthService } from 'src/app/_services/ng-auth.service';
   styleUrls: ['./edit-user.component.css']
 })
 export class EditUserComponent implements OnInit {
+  user: any
 
-  constructor(public ngAuthService: NgAuthService) { }
+  constructor(public ngAuthService: NgAuthService, public afAuth: AngularFireAuth, public userService: UsersService, public router: Router) { }
 
   ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => {
+      if (user && user.uid) {
+        this.user = user;
+      };
+    });
+  }
+
+  UpdateUserName(displayName: string) {
+    updateProfile(this.user, { displayName: displayName }).then(() => {
+      this.userService.updateUsername(displayName, this.user.uid)
+      this.router.navigate(['dashboard']);
+        Swal.fire('Bravo', 'Votre profil a été modifié', 'success');
+      }).catch((error) => {
+        Swal.fire('Erreur', error, 'error');
+      })
   }
 
 }
